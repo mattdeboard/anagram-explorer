b0d4ba720b26e33e7d0d20f820b6b774328105e1
Matt DeBoard <matt.deboard@gmail.com>
2022-04-16 16:38:22 -0500

    Make the pasting and typing work.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,84 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { ChangeEvent } from "react";
 import "./App.css";
+import { isEqual } from "lodash";
+import { Button, Container } from "reactstrap";
+import { InputBox } from "./components";
 
 function App() {
+  const [source, setSource] = React.useState<string>("");
+  const [sourceIndex, setSourceIndex] = React.useState<Record<string, number>>(
+    characterIndex(source),
+  );
+  const [input, setInput] = React.useState<string>("");
+  const [inputIndex, setInputIndex] = React.useState<Record<string, number>>(
+    characterIndex(input),
+  );
+  const [inputDisabled, setInputDisabled] = React.useState(true);
+
+  const handleInput = React.useCallback(
+    (event: ChangeEvent<HTMLInputElement>) => {
+      const proposedInput = event.target.value;
+
+      if (
+        !proposedInput.length ||
+        Object.keys(sourceIndex).includes(
+          proposedInput[proposedInput.length - 1],
+        )
+      ) {
+        setInput(proposedInput);
+        setInputIndex(characterIndex(proposedInput));
+      }
+    },
+    [sourceIndex, setInput, setInputIndex],
+  );
+  React.useEffect(() => {
+    const newIndex = characterIndex(input);
+    if (!isEqual(newIndex, inputIndex)) {
+      setInputIndex(newIndex);
+    }
+  }, [input, setInputIndex, inputIndex, inputDisabled]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <Container>
+        <InputBox content={source} disabled={true} />
+        <InputBox
+          content={input}
+          disabled={inputDisabled}
+          setContent={handleInput}
+        />
+        <Button
+          color="primary"
+          onClick={async () => {
+            const contents = await navigator.clipboard.readText();
+
+            if (contents.length) {
+              setSource(contents);
+              setSourceIndex(characterIndex(contents));
+              setInputDisabled(false);
+            }
+          }}
+          size="lg"
         >
-          Learn React
-        </a>
-      </header>
+          Paste clipboard contents
+        </Button>
+      </Container>
     </div>
   );
 }
 
+function characterIndex(s: string) {
+  const index: Record<string, number> = {};
+  for (const c of s) {
+    // This is unusual convention to me, but apparently some harmful bugs can come from invoking
+    // `hasOwnProperty` directly. I don't think this code is a case where this would wind up being
+    // an issue, but it's a novel-enough thing to learn that I'm leaving it changes to help me
+    // remember.
+    //
+    // https://eslint.org/docs/rules/no-prototype-builtins for more info.
+    index[c] = Object.prototype.hasOwnProperty.call(index, c)
+      ? index[c] + 1
+      : 1;
+  }
+  return index;
+}
 export default App;
--- src/components/InputBox.tsx
+++ src/components/InputBox.tsx
@@ -1,5 +1,34 @@
 import React from "react";
+import { FormGroup, Label, Input, Form } from "reactstrap";
 
-export default function InputBox() {
-  return <div></div>;
+export default function InputBox({
+  content = "",
+  disabled,
+  setContent,
+}: {
+  content?: string;
+  disabled: boolean;
+  setContent?: React.ChangeEventHandler;
+}) {
+  return (
+    <div>
+      <Form>
+        <FormGroup>
+          <Label for="inputArea">Text Area</Label>
+          <Input
+            disabled={disabled}
+            id="inputArea"
+            name="text"
+            onChange={event => {
+              if (setContent) {
+                setContent(event);
+              }
+            }}
+            type="textarea"
+            value={content}
+          />
+        </FormGroup>
+      </Form>
+    </div>
+  );
 }
